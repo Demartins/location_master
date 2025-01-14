0e4769149b2491b41c28b13cf391d30730f725e0
Diego Aparecido Martins <diego.a_martins@hotmail.com>
2025-01-14 09:18:15 -0300

    update topics name

--- src/main/java/br/com/diego/poc/events/Consumer.java
+++ src/main/java/br/com/diego/poc/events/Consumer.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
-    @KafkaListener(topics = "./bin/zkCli.sh -server localhost:2181", groupId = "group-1")
+    @KafkaListener(topics = "intercab.squad.locationref.topic.internal.any.v1", groupId = "group-1")
     public void receiveMessage(String message) {
         // Process the received message
         System.out.println("Consumer Message: " + message);
