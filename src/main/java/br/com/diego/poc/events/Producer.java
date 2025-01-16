904c3ba0696935e7add10cbfee33a1ca985dbbf0
DiegoMartins <diego.aparecido.martins1@ibm.com>
2025-01-16 17:15:11 -0300

    improvements

--- src/main/java/br/com/diego/poc/events/Producer.java
+++ src/main/java/br/com/diego/poc/events/Producer.java
@@ -1,17 +1,23 @@
 package br.com.diego.poc.events;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Producer {
+
+    @Value("${kafka.topic}")
+    private String topic;
+
     private final KafkaTemplate<String, String> kafkaTemplate;
+
 
     public Producer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("intercab.squad.locationref.topic.internal.any.v1", message);
+        kafkaTemplate.send(topic, message);
     }
 }
